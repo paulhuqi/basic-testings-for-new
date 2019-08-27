7ed2c3ba37f3a315c02fabfa1dda43865130e325
paulhuqi <paulhuqi@gmail.com>
2019-08-27 11:58:29 +0800

    StringsTest

---
+++ src/main/java/others/Strings.java
@@ -0,0 +1,26 @@
+package others;
+
+public class Strings {
+    public static void main(String[] args) {
+        stringPlusCompareStrArr();
+    }
+    
+    private static void stringPlusCompareStrArr() {
+        String idNo = "idNo";
+        String idType = "idType";
+        String[] idArr = {idNo, idType};
+        int testTimes = 1000000;
+        long startTime1 = System.currentTimeMillis();
+        for (int i = 0; i < testTimes; i++) {
+            idArr.hashCode();
+        }
+        System.out.println("idArr.hashCode() time taken: " + (System.currentTimeMillis() - startTime1));
+        
+        long startTime2 = System.currentTimeMillis();
+        String str = null;
+        for (int i = 0; i < testTimes; i++) {
+            str = idNo + idType;
+        }
+        System.out.println("idNo + idType time taken: " + (System.currentTimeMillis() - startTime2));
+    }
+}
